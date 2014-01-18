ef32fea7c0f6d51096352081e44190064e549e08
Erik Nilsson <erik.d.nilsson@gmail.com>
2014-01-18 17:31:59 -0500

    Fixat slider.js

--- Portfolio/slider.js
+++ Portfolio/slider.js
@@ -2,13 +2,13 @@ var i = 0;
 // Skapar en Array med 3st "lådor", en för varje bild.
 var image = [];
 image[0] = "ank1.jpg";
-image[1] = ".jpg";
-image[2] = ".jpg";
+image[1] = "ank2.jpg";
+image[2] = "ank3.jpg";
 
 // window.onload = När sidan har laddats, kör funktionen.
 window.onload = function swapImage() { 
-    $("#section1").css("background-image", image[i]); 
-    setTimeout(swapImage,3000);
+    $("#section1").css("background-image", "url("+image[i] +")"); 
+    setTimeout(swapImage,5000);
     // Så länge i(0) är mindre än arrayboxen plussar den på 1.
     if (i < image.length -1)
     {
